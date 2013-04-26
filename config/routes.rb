e52d7b3fe369a203c9cd874181e489f1abf2f70e
Nathan Schuett <nschuett@gmail.com>
2013-04-26 10:13:10 -0700

    Update routes to new home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Carelink::Application.routes.draw do
+  root :to => 'pages#home'
+
+  #get "pages/home"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
