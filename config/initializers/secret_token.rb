088147c746ee69459c7124af99f765c0ea2e9dfc
Nathan Schuett <nschuett@gmail.com>
2013-04-26 09:40:56 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Carelink::Application.config.secret_token = '5b3009910beacfc8c776d826607fa23c466e94a17ac0a0a88f2249b851a7fcb8e3820fb317696e9fb05d3e60cc621364394d945f488e16de59220e571cb96c1a'
